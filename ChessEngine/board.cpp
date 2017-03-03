d9f9d393ecc642ef354c331b200a6ef6bba83ff8
Joshua <Joshuajames355@gmail.com>
2017-03-03 17:02:44 +0000

    Added board class using bitboards and created printboard function.

---
+++ ChessEngine/board.cpp
@@ -0,0 +1,98 @@
+#include "board.h"
+
+
+board::board()
+{
+}
+
+void board::defaults()
+{
+	whitePawnBitboard = rank1;
+	whiteRookBitboard = 129; //2^0 + 2^7
+	whiteKnightBitboard = 66; //2^1 + 2^6
+	whiteBishopBitboard = 36; //2^2 + 2^5
+	whiteQueenBitboard = 8; //2^3
+	whiteKingBitboard = 16; //2^4
+
+	blackPawnBitboard = rank6;
+	blackRookBitboard = 9295429630892703744; //2^56 + 2^63
+	blackKnightBitboard = 4755801206503243776; //2^57 + 2^62
+	blackBishopBitboard = 2594073385365405696; //2^58 + 2^61
+	blackQueenBitboard = 576460752303423488; //2^59
+	blackKingBitboard = 1152921504606846976; //2^60
+}
+
+void board::printBoard()
+{
+	int counter = 56;
+	for (int x = 0; x < 8; x++)
+	{
+		for (int y = 0; y < 8; y++)
+		{
+
+			int64_t currentPosBitboard = (int64_t)1 << counter;
+
+			//Checking for white pieces
+			if ((whitePawnBitboard & currentPosBitboard) != 0) //If the piece is a white pawn;
+			{
+				std::cout << "|" << "WP";
+			}
+			else if ((whiteRookBitboard & currentPosBitboard) != 0) //If the piece is a white rook;
+			{
+				std::cout << "|" << "WR";
+			}
+			else if ((whiteKnightBitboard & currentPosBitboard) != 0) //If the piece is a white knight;
+			{
+				std::cout << "|" << "WN";
+			}
+			else if ((whiteBishopBitboard & currentPosBitboard) != 0) //If the piece is a white bishop;
+			{
+				std::cout << "|" << "WB";
+			}
+			else if ((whiteQueenBitboard & currentPosBitboard) != 0) //If the piece is a white queen;
+			{
+				std::cout << "|" << "WQ";
+			}
+			else if ((whiteKingBitboard & currentPosBitboard) != 0) //If the piece is a white king;
+			{
+				std::cout << "|" << "WK";
+			}
+
+			//Checking for black pieces
+			else if ((blackPawnBitboard & currentPosBitboard) != 0) //If the piece is a black pawn;
+			{
+				std::cout << "|" << "BP";
+			}
+			else if ((blackRookBitboard & currentPosBitboard) != 0) //If the piece is a black rook;
+			{
+				std::cout << "|" << "BR";
+			}
+			else if ((blackKnightBitboard & currentPosBitboard) != 0) //If the piece is a black knight;
+			{
+				std::cout << "|" << "BN";
+			}
+			else if ((blackBishopBitboard & currentPosBitboard) != 0) //If the piece is a black bishop;
+			{
+				std::cout << "|" << "BB";
+			}
+			else if ((blackQueenBitboard & currentPosBitboard) != 0) //If the piece is a black queen;
+			{
+				std::cout << "|" << "BQ";
+			}
+			else if ((blackKingBitboard & currentPosBitboard) != 0) //If the piece is a black king;
+			{
+				std::cout << "|" << "BK";
+			}
+			else
+			{
+				std::cout << "|" << "  ";
+			}
+			counter++;
+		}
+		std::cout << "|\n";
+		counter -= 16;
+	}
+
+}
+
+
---
+++ ChessEngine/board.h
@@ -0,0 +1,52 @@
+#pragma once
+#include <stdint.h>
+#include <cmath>
+#include <iostream>
+
+#define emptyBitboard 0
+#define universalBitboard 18446744073709551615 //2**64 - 1
+
+#define rank0 255
+#define rank1 65280
+#define rank2 16711680
+#define rank3 4278190080
+#define rank4 1095216660480
+#define rank5 280375465082880
+#define rank6 71776119061217280
+#define rank7 18374686479671623680
+
+#define fileA 9259542123273814144
+#define fileB 4629771061636907072
+#define fileC 2314885530818453536
+#define fileD 1157442765409226768
+#define fileE 578721382704613384
+#define fileF 289360691352306692
+#define fileG 144680345676153346
+#define fileH 72340172838076673
+
+class board
+{
+public:
+	board();
+	void defaults();
+	void printBoard();
+
+	int64_t whitePawnBitboard;
+	int64_t whiteKnightBitboard;
+	int64_t whiteBishopBitboard;
+	int64_t whiteRookBitboard;
+	int64_t whiteQueenBitboard;
+	int64_t whiteKingBitboard;
+	
+	int64_t blackPawnBitboard;
+	int64_t blackKnightBitboard;
+	int64_t blackBishopBitboard;
+	int64_t blackRookBitboard;
+	int64_t blackQueenBitboard;
+	int64_t blackKingBitboard;
+
+	int64_t whitePieces;
+	int64_t blackPieces;
+	int64_t allPieces;
+};
+
