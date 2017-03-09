ef4ea5e8c8affe23069cbc746f866c67e3b5159e
Administrator <admin@example.com>
2017-03-09 11:40:00 +0000

    Added function definition to moveGeneration.h

--- ChessEngineLibrary/moveGeneration.h
+++ ChessEngineLibrary/moveGeneration.h
@@ -11,5 +11,6 @@ std::vector<Move> searchForMoves(Board* board, colours aiColour);
 
 void generatePawnMoves(Board* board ,colours aiColour, std::vector<Move>& Movelist);
 void generateKingMoves(Board* board, colours aiColour, std::vector<Move>& Movelist);
+void generateKnightMoves(Board * board, colours aiColour, std::vector<Move>& Movelist)
 
 void addMoves(int start, int end, colours aiColour, pieceType piece , std::vector<Move>& Movelist , Board* board);
